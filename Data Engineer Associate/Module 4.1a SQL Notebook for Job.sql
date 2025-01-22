1da32c87c93881ca70778476e29fc4583df9465c
junshan0 <junshan1@yahoo.com>
2025-01-22 10:43:46 -0500

    Delete old Data Engineer Associate directory

--- Data Engineer Associate/Module 4.1a SQL Notebook for Job.sql
+++
@@ -1,2 +0,0 @@
--- Databricks notebook source
-SELECT count(*) FROM dataatexternallocation.default.world_population
--- Data Engineer Associate/Module 4.1b Error Out.sql
+++
@@ -1,7 +0,0 @@
--- Databricks notebook source
--- This notebook contains an intentional mistake to demonstrate error handling
-SELECT err FROM dataatexternallocation.default.world_population; 
-
--- COMMAND ----------
-
-CREATE FUNCTION square(x DOUBLE) RETURNS DOUBLE RETURN area(x, x);
